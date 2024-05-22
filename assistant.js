2c7d52d20431207e689aa0839f90ff1c80236b5c
Jasper Ebeli <jasperebeli@hotmail.nl>
2024-05-22 23:01:39 +0200

    User Threads Done

---
+++ assistant.js
@@ -0,0 +1,112 @@
+const OpenAI = require("openai");
+
+class OpenAiManager {
+    constructor(newAssistant=false) {
+        this.model = "gpt-4o";
+        this.myAssistant = null;
+
+        const API_KEY = process.env.OPENAI_API_KEY;
+        const ASSIST_ID = process.env.OPENAI_ASSISTANT_ID;
+        if (!API_KEY) {
+            console.error("No OPENAI_API_KEY");
+            process.exit(1);
+        }
+
+        var fs = require('fs');
+        this.instructions = fs.readFileSync('assistant.txt', 'utf8');
+        this.threadInstructions = fs.readFileSync('thread.txt', 'utf8');
+
+        try {
+            this.client = new OpenAI(API_KEY);
+            if (ASSIST_ID) {
+                this.myAssistant = this.getAssistant(ASSIST_ID);
+            } else {
+                console.error("No Assistant");
+                process.exit(1);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            throw error;
+        }
+    }
+
+    async createAssistant() {
+        return this.client.beta.assistants.create({
+            instructions: this.instructions,
+            name: "BlisAI Assistant",
+            tools: [],
+            model: this.model,
+            temperature: 0.15,
+            top_p: 1,
+            response_format: {"type": "json_object"}
+        });
+    }
+
+    async listAssistants() {
+        return this.client.beta.assistants.list({
+            order: "desc",
+            limit: "20",
+        });
+    }
+
+    async getAssistant(assistantId) {
+        return this.client.beta.assistants.retrieve(assistantId);
+    }
+
+    async deleteAssistant(assistantId) {
+        return this.client.beta.assistants.del(assistantId);
+    }
+
+    async deleteAllAssistants() {
+        const assistants = await this.listAssistants();
+        for (const assistant of assistants.data) {
+            await this.deleteAssistant(assistant.id);
+        }
+    }
+
+    async startThread() {
+        try {
+            const newThread = await this.client.beta.threads.create();
+            return newThread.id;
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
+        }
+    }
+
+    async getThread(threadId) {
+        return this.client.beta.threads.retrieve(threadId);
+    }
+
+    async deleteThreads(threadId) {
+        return this.client.beta.threads.del(threadId);
+    }
+
+    async createMessage(threadId, message) {
+        this.client.beta.threads.messages.create(threadId,
+            {role: "user", content: message}
+        );
+        return await this.client.beta.threads.runs.createAndPoll(
+            threadId,
+            {
+                assistant_id: (await this.myAssistant).id,
+                instructions: this.threadInstructions
+            }
+        );
+    }
+
+    async listMessages(threadId) {
+        return this.client.beta.threads.messages.list(threadId);
+    }
+
+    async getMessage(threadId, messageId) {
+        return this.client.beta.threads.messages.retrieve(threadId, messageId);
+    }
+
+    async deleteMessage(threadId, messageId) {
+        return this.client.beta.threads.messages.del(threadId, messageId);
+    }
+
+}
+
+module.exports = {OpenAiManager};
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const json = require('body-parser').json;
-const {OpenAiManager} = require('./chat.js');
+const {OpenAiManager} = require('./assistant.js');
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
+const manager = new OpenAiManager();
 
 app.use(json());
 
-app.post('/send', async (req, res) => {
+app.post('/start', async (req, res) => {
     try {
         const { data } = req.body;
-        const manager = new OpenAiManager();
-        const response = await manager.chat(data);
-        res.json({ message: response });
+        const thread = await manager.startThread();
+        manager.createMessage(thread, data).then(r => {
+            const response = r.data[0];
+            const responseValue = response.content[0].text.value;
+            res.json({ message: responseValue, threadId: thread });
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './templates' });
+app.post('/chat', async (req, res) => {
+    try {
+        const { threadId, data } = req.body;
+        manager.createMessage(threadId, data).then(r => {
+            const response = r.data[0];
+            const responseValue = response.content[0].text.value;
+            res.json({ message: responseValue });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(port, () => {
