0f497a4d33d8d51cd12612d93c66acd629decccd
Jasper Ebeli <jasperebeli@hotmail.nl>
2024-05-26 19:57:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -54,51 +54,6 @@ app.post('/chat/:id', async (req, res) => {
     }
 });
 
-
-const dummyData = {
-    "name": "Noble Desiree Grey Matt",
-    "aantal_m2": 5,
-    "prijs_per_m2": 247.52,
-    "prijs_m2_totaal": 1237.6,
-    "randafwerking_m": 5,
-    "randafwerking_prijs_per_m": 87,
-    "randafwerking_hoogte_mm": 50,
-    "randafwerking_prijs_totaal": 435,
-    "spatrand_m": 3,
-    "spatrand_prijs_per_m": 35,
-    "spatrand_hoogte_mm": 100,
-    "spatrand_prijs_totaal": 105,
-    "vensterbank_m": 3,
-    "vensterbank_prijs_per_m": 35,
-    "vensterbank_breete_mm": 100,
-    "vensterbank_prijs_totaal": 105,
-    "spoelbak": true,
-    "uitsparing_spoelbak": "ruw",
-    "spoelbak_prijs": 70,
-    "kraangat": true,
-    "kraangat_prijs": 10.70,
-    "zeepdispenser": true,
-    "zeepdispenser_prijs": 10.70,
-    "boorgaten": true,
-    "boorgaten_stuk": 1,
-    "bootgaten_mm": 5,
-    "boorgaten_prijs_per_stuk": 5,
-    "bootgaten_prijs_totaal": 5,
-    "WCD": true,
-    "WCD_prijs": 13.50,
-    "achterwand": true,
-    "acherwand_m2": 5,
-    "achterwand_prijs_per_m2": 309.40,
-    "achterwand_prijs_totaal": 1547,
-    "offerte_prijs_totaal": 3761
-}
-
-async function verifyTest(data) {
-    await manager.verify(data).then((responseValue) => {
-        console.log({message: responseValue});
-    });
-}
-
 async function sendMessageAndReceiveJSON(threadId, message) {
     await manager.createMessage(threadId, message);
     await manager.runThread(threadId).then(async r => {
